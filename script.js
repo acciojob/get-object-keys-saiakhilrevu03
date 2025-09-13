ee0bc7d9c376b247a16de9be4c197c4326a4283f
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-09-13 21:45:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// Create the student object
+const student = {
+  name: "John",
+};
+
+// Add getKeys method to all objects
+Object.prototype.getKeys = function () {
+  return Object.keys(this);
+};
+
+
+
 const myObj = { name: "John", age: 30, city: "New York" };
 console.log(myObj.getKeys()); // ["name", "age", "city"]
 
